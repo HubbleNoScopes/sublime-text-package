05a54e20eaa2706a06bc74b218101c7e6ffb29c5
Dallas <151665756+HubbleNoScopes@users.noreply.github.com>
2023-11-24 11:25:13 -0800

    Update YourMacro.js

--- YourMacro.js
+++ YourMacro.js
@@ -1,20 +1,3 @@
-// Function to validate single-quoted string literals
-function validateSingleQuotedString(value) {
-  try {
-    // Attempt to evaluate the provided string as a JavaScript expression
-    eval(`let testValue = ${value}`);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
-
-// Function to validate double-quoted string literals
-function validateDoubleQuotedString(value) {
-  // Check if the value inside double quotes contains only alphanumeric characters
-  return /^"[\w\s]*"$/.test(value);
-}
-
 const fs = require('fs');
 const sublime = require('sublime');
 
@@ -30,7 +13,7 @@ function convertStringLiterals(filePath) {
     // Check if it's a single-quoted literal
     if (quote === "'") {
       if (!validateSingleQuotedString(content)) {
-        // Display an error message without making changes
+        // Display error message without modifying the content
         hasErrors = true;
         sublime.error_message(`Invalid single-quoted string literal: ${content}`);
         return match;
@@ -38,7 +21,7 @@ function convertStringLiterals(filePath) {
     } else {
       // Check if it's a double-quoted literal
       if (!validateDoubleQuotedString(content)) {
-        // Display an error message without making changes
+        // Display error message without modifying the content
         hasErrors = true;
         sublime.error_message(`Invalid double-quoted string literal: ${content}`);
         return match;
@@ -49,10 +32,7 @@ function convertStringLiterals(filePath) {
   });
 
   if (!hasErrors) {
-    sublime.message_dialog('String literals converted successfully!');
-    // Replace the content in the active Sublime Text view
-    const activeView = sublime.active_window().active_view();
-    activeView.run_command("insert", {"characters": fileContent});
+    sublime.message_dialog('String literals are valid.');
   }
 }
 
